bd8f0a59866d66b21d15b8b6ffc2c959de548240
DavidPerezOrfila <knightbcn@gmail.com>
2017-05-11 20:41:13 +0200

    add gameController

--- src/AppBundle/Controller/gameController.php
+++ src/AppBundle/Controller/gameController.php
@@ -5,3 +5,20 @@
  * Date: 11/05/17
  * Time: 19:35
  */
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class gameController extends Controller
+{
+    /**
+     * @Route("/Games", name="app_games_index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+
+        return $this->render(':Games:index.html.twig');
+    }
+}
